c12ffcab2413f90eec99d4d13bd4f475d12b3f38
Japor Mejoym <jonathan.mejoy@gmail.com>
2023-03-27 16:06:38 +0800

    RCT-328 : Whats New in React

---
+++ Whats-New-React/student-form/src/components/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import React, { useState, Component } from "react";
+
+export class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>Error occured!</h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
